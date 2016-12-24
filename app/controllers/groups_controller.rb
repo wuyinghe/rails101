79235aefb021a9fc6610e3f09e4507a6f7d036fa
wuyinghe <wuyinghe1925@gmail.com>
2016-12-24 13:57:40 +0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,9 +28,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-
-    find_group_and_check_permission
-
    if @group.update(group_params)
      redirect_to groups_path, notice: "Update Success"
    else
@@ -39,8 +36,6 @@ class GroupsController < ApplicationController
  end
 
    def destroy
-     find_group_and_check_permission
-
      @group.destroy
      flash[:alert] = "Group deleted"
      redirect_to groups_path
